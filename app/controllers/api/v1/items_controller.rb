e18615e033a0df19884a22097553c42178ccb1fb
djtrujillo <danny.j.trujillo@gmail.com>
2017-11-01 11:32:38 -0600

    Trying to create new item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,29 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    @items = Item.all.map do |item|
-      render json: ItemDecorator.new.all
-    end
+    render json: Item.all
+    # @items = Item.all.map do |item|
+    #   render json: ItemDecorator.new.all
+    # end
   end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    render json: @item.delete
+  end
+
+  def create
+    binding.pry
+    render json: Item.create(item_params)
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :image_url)
+    end
 
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -13,7 +13,40 @@ describe "Items API" do
     expect(items.count).to eq(3)
   end
 
-  
+  it "can return one item" do
+    id = create(:item).id
 
+    get "/api/v1/items/#{id}"
 
+    expect(response).to be_success
+
+    item = JSON.parse(response.body)
+
+    expect(item["id"]).to eq(id)
+  end
+
+  it "can delete an item" do
+    id = create(:item).id
+    expect(Item.all.count).to eq(1)
+
+    delete "/api/v1/items/#{id}"
+
+    expect(response).to be_success
+
+    expect(Item.count).to be(0)
+  end
+
+  it "can create new item" do
+    item_params = {name: "item", description: "description", image_url: "www.example.com" }
+
+    post '/api/v1/items', params: {item: item_params} 
+
+    expect(response).to be_success
+
+    item = JSON.parse(response.body)
+
+    expect(item.name).to eq("item")
+    expect(item.description).to eq("description")
+    expect(item.image_url).to eq("www.example.com")
+  end
 end
